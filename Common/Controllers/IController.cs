24a254fe0ba4806455be9f0df2f403d32f54808d
galahirudilhara <galahirudilhara@gmail.com>
2025-01-03 18:41:38 +0530

    add the constants to the IController

--- Common/Controllers/IController.cs
+++ Common/Controllers/IController.cs
@@ -5,6 +5,9 @@ namespace Controllers
 {
     public abstract partial class IController : Node
     {
+        public int Gravity = 980;
+
+        public int InAirProjectionSpeed = 150;
         public abstract MoveSpec WantToMove();
         public abstract JumpSpec WantToJump();
     }
