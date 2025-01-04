e50c850af611d1798a3e529c8d563316c9650ad6
galahirudilhara <galahirudilhara@gmail.com>
2025-01-04 23:22:53 +0530

    created the scene manager and connected it with the loading animation

--- Globals/SceneManager.cs
+++ Globals/SceneManager.cs
@@ -36,10 +36,16 @@ namespace Globals
 
         public async void LoadScene(string scenePath, Node superNode = null)
         {
-            var loadingScene = LoadingScrene.Instantiate();
+            LoadingScene loadingScene = (LoadingScene)LoadingScrene.Instantiate();
 
             // Show the loading screen while loading the resources
             GetTree().Root.CallDeferred("add_child", loadingScene);
+
+            // await until the loading scene is added to the tree
+            await ToSignal(loadingScene, "ready");
+
+            // If added then play the animation
+            await loadingScene.PlayAnimation("slide_in");
 
             // If supply super node then set it
             superNode ??= GetTree().Root;
@@ -47,7 +53,7 @@ namespace Globals
             // clear current scene if there is any scene loaded
             CurrentScene?.QueueFree();
 
-            // var loadedScene = GD.Load<PackedScene>(scenePath);
+            // Run code asyncronously to load the new scene
             var loadedScene = await Task.Run(() => GD.Load<PackedScene>(scenePath));
             if (loadedScene == null)
             {
@@ -55,9 +61,17 @@ namespace Globals
                 return;
             }
 
-            // Instanciate the loaded Scene
+            // Instanciate the new Scene
             CurrentScene = loadedScene.Instantiate();
             superNode.CallDeferred("add_child", CurrentScene);
+
+            // Wait until the new scene is successfully loaded and added to the tree
+            await ToSignal(CurrentScene, "ready");
+
+            // If the loadedScene is added to the tree then run the next animation
+            await loadingScene.PlayAnimation("slide_out");
+
+            // Remove the loading scene after the new scene successfully loaded
             GetTree().Root.CallDeferred("remove_child", loadingScene);
         }
     }
---
+++ UI/LoadingScene/LoadingScene.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+using System.Threading.Tasks;
+
+public partial class LoadingScene : CanvasLayer
+{
+	private AnimationPlayer animationPlayer;
+	public override void _Ready()
+	{
+		this.animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+	}
+
+	public async Task PlayAnimation(string animationName)
+	{
+		this.animationPlayer.Play(animationName);
+		var tcs = new TaskCompletionSource<bool>();
+
+		animationPlayer.AnimationFinished += (animName) =>
+		{
+			if (animName == animationName)
+			{
+				tcs.SetResult(true);
+			}
+		};
+		await tcs.Task;
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
