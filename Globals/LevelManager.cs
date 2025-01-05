32502f259ff40e7ce9eae4d5ed52046d2915d28b
galahirudilhara <galahirudilhara@gmail.com>
2025-01-05 18:31:11 +0530

    add the commens and next level and previous level loading methods to the level Manager

--- Globals/LevelManager.cs
+++ Globals/LevelManager.cs
@@ -17,6 +17,12 @@ namespace Globals
 
         private List<string> LevelScenes = new List<string> { "res://Levels/Level1/Level1.tscn" };
 
+        // This was initialized by the level Manager using the saved data
+        public void Initialized(int CurrentLevel)
+        {
+            this.CurrentLevelIndex = CurrentLevel - 1;
+        }
+
         public override void _Ready()
         {
             base._Ready();
@@ -30,11 +36,21 @@ namespace Globals
 
         }
 
+        /// <summary>
+        /// This method is responsible for loading specified level. If the level number is greater than the available levels, it loads the last level. If it is less than 1, it loads the first level.
+        /// </summary>
+        /// <param name="LevelNumber"></param>
         public void LoadLevel(int LevelNumber)
         {
             if (LevelNumber > LevelScenes.Count)
             {
+                GD.PrintErr($"There is no level number {LevelNumber}");
                 LevelNumber = LevelScenes.Count;
+            }
+            if (LevelNumber < 1)
+            {
+                GD.PrintErr($"There is no level number {LevelNumber}");
+                LevelNumber = 0;
             }
             var LevelIndex = LevelNumber - 1;
 
@@ -44,11 +60,17 @@ namespace Globals
             SceneManager.Instance.LoadScene(levelScenePath);
         }
 
+        /// <summary>
+        /// This method is responsible for reloading the current Level
+        /// </summary>
         public void ReloadLevel()
         {
             SceneManager.Instance.LoadScene(LevelScenes[CurrentLevelIndex]);
         }
 
+        /// <summary>
+        /// This method is used for loading the next level. If there is no level remaining, it will sends the GameFinished signal
+        /// </summary>
         public void LoadNextLevel()
         {
             var nextLevelIndex = this.CurrentLevelIndex + 1;
@@ -57,6 +79,22 @@ namespace Globals
                 MessageBus.Instance.EmitGameFinished();
                 return;
             }
+            this.CurrentLevelIndex = nextLevelIndex;
+            SceneManager.Instance.LoadScene(LevelScenes[CurrentLevelIndex]);
+        }
+
+        /// <summary>
+        /// This method is used to load the previous level.
+        /// </summary>
+        public void LoadPreviousLevel()
+        {
+            var previousLevelIndex = this.CurrentLevelIndex - 1;
+            if (previousLevelIndex < 0 || previousLevelIndex >= this.LevelScenes.Count)
+            {
+                GD.PrintErr("There is no previous level remaining");
+                return;
+            }
+            this.CurrentLevelIndex = previousLevelIndex;
             SceneManager.Instance.LoadScene(LevelScenes[CurrentLevelIndex]);
         }
     }
