66f90cbab2c8103a9d3eae469a3f4eb0daa23ca4
galahirudilhara <galahirudilhara@gmail.com>
2025-01-05 15:03:49 +0530

    add the health bar hue changing feature based on the progress

--- Common/Components/Display/HealthProgress/HealthProgress.cs
+++ Common/Components/Display/HealthProgress/HealthProgress.cs
@@ -24,11 +24,27 @@ public partial class HealthProgress : Control
 
 	private void OnProgressChange(int currentHitpoints)
 	{
-		this.progressBar.Value = CalProgress(currentHitpoints);
+		// Set the current progress
+		int CurrentProgress = CalProgress(currentHitpoints);
+		this.progressBar.Value = CurrentProgress;
+
+		// Set the progress bar color
+		var styleBox = progressBar.GetThemeStylebox("fill", "ProgressBar") as StyleBoxFlat;
+		if (styleBox != null)
+		{
+			styleBox.BgColor = Color.FromHsv(ProgressHue(CurrentProgress), 1f, 0.8f, 1f);
+		}
 	}
 
 	private int CalProgress(int currentHitpoints)
 	{
 		return currentHitpoints * 100 / hitpoint.MaxHitpoints;
+	}
+	private float ProgressHue(int currentProgress)
+	{
+		int MaxHue = 130;
+		int MaxPercentage = 100;
+		int HueValue = MaxHue * currentProgress / MaxPercentage;
+		return HueValue / 360.0f;
 	}
 }
