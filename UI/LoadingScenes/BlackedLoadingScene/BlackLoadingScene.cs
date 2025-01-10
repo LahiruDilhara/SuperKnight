399fb0ac66178323e9d2e2756f56d6026d2fb6e9
galahirudilhara <galahirudilhara@gmail.com>
2025-01-10 13:02:53 +0530

    update the BlackLoadingScene with async

--- UI/LoadingScenes/BlackedLoadingScene/BlackLoadingScene.cs
+++ UI/LoadingScenes/BlackedLoadingScene/BlackLoadingScene.cs
@@ -5,23 +5,31 @@ using Types;
 public partial class BlackLoadingScene : LoadingScene
 {
 	private AnimationPlayer animationPlayer;
+	private TaskCompletionSource<bool> _currentAnimationTask;
 	public override void _Ready()
 	{
 		this.animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+		animationPlayer.AnimationFinished += OnAnimationFinished;
 	}
 
 	public override async Task PlayAnimation(string animationName)
 	{
-		this.animationPlayer.Play(animationName);
-		var tcs = new TaskCompletionSource<bool>();
-
-		animationPlayer.AnimationFinished += (animName) =>
+		if (_currentAnimationTask != null && !_currentAnimationTask.Task.IsCompleted)
 		{
-			if (animName == animationName)
-			{
-				tcs.SetResult(true);
-			}
-		};
-		await tcs.Task;
+			GD.PrintErr("Previous animation task is still running.");
+			return;
+		}
+		_currentAnimationTask = new TaskCompletionSource<bool>();
+		this.animationPlayer.Play(animationName);
+
+		await _currentAnimationTask.Task;
+	}
+
+	private void OnAnimationFinished(StringName finishedAnimationName)
+	{
+		if (_currentAnimationTask != null && !_currentAnimationTask.Task.IsCompleted)
+		{
+			_currentAnimationTask.SetResult(true);
+		}
 	}
 }
